f4b010b60b04a181459d13d34993413a8ed3aec2
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-01-01 15:24:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,31 @@
 import base64, codecs
-print('Deobfuscation Starting....')
-# open the text file in read mode
-
-magic = 'cHJpbn'
-love = 'DbW3Il'
-god = 'IHNleH'
-destiny = 'xaXD=='
-
-
+import time
+# Dont touch
 joy = 'rot13'
 
-
-
-
-
-
-rot = codecs.decode(love, joy)
-rots = codecs.decode(destiny, joy)
-
-trust = magic + rot + god + rots
-s = base64.b64decode(trust)
-
-
-# decode the decoded text using the detected encoding
-output = s.decode("utf-8")
-print('Script hidden in it was : \n')
-
-print(output + '\n')
-# write the decoded text to a file
-import base64, codecs
 print('Deobfuscation Starting....')
-# open the text file in read mode
-magic = 'ZnJvbSBzeXMgaW1wb3J0IGFyZ3YsIGV4aXQKCmZsYWdzID0gWwoJImZsYWd7UGFyYW1fU2lkZGhhcnRoJ3NfTXlfT'
-love = 'zSgMK0vYNbWVyOupzSgK1AcMTEbLKW0nPqmK015K05uoJHvPy0XPzyzVTkyovuupzq2XFN+VQR6PtyzoTSaVQ0tLK'
-god = 'JndlsxXQplbHNlOgoJZmxhZyA9IGlucHV0KCdFbnRlciB0aGUgZmxhZzogJykKCmZvciBmIGluIGZsYWdzOgoJaWY'
-destiny = 'tMvN9CFOzoTSaBtbWPKOlnJ50XPqQo3WlMJA0VFpcPtxWMKucqPtjXDbXpUWcoaDbW1qlo25aVFpcPzI4nKDbZFx='
 
+# It will open obfuscated.txt and check everyline so magic love god and destiny
+with open('obfuscated.txt', 'r') as file:
+    first_line = file.readline()
+    second = file.readline()
+    third = file.readline()
+    fourth = file.readline()
 
+    a = first_line[9:-2]
+    b = second[8:-2]
+    c = third[7:-2]
+    d = fourth[11:-1]
 
-joy = 'rot13'
+# Will decode with rot13
+rot = codecs.decode(b, joy)
+rots = codecs.decode(d, joy)
 
+# Will make everything in 1 variable
+trust = a + rot + c + rots
 
-
-
-
-
-rot = codecs.decode(love, joy)
-rots = codecs.decode(destiny, joy)
-
-trust = magic + rot + god + rots
+# Will decode in base64
 s = base64.b64decode(trust)
-
 
 # decode the decoded text using the detected encoding
 output = s.decode("utf-8")
@@ -59,27 +34,9 @@ print('Script hidden in it was : \n')
 print(output + '\n')
 # write the decoded text to a file
 
-input('Press enter to save it in the file (Copy the text from here if there is any error')
-
+input('Press enter to save it in the file (Copy the text from here if there is any error)')
 with open('deobfuscated.txt', 'w+') as f:
     f.write(output)
-
-import time
-
-text = "Deobfuscation Ended Saved in deobfuscated.txt"
-
-
-for char in text:
-    print(char, end='', flush=True)
-    time.sleep(0.01)
-
-print()
-input('Press any key to close...')
-
-with open('deobfuscated.txt', 'w+') as f:
-    f.write(output)
-
-import time
 
 text = "Deobfuscation Ended Saved in deobfuscated.txt"
 
